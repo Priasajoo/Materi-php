a11ca9a5e4ab4e3ca869bb7cac13992a4e42d14d
priasajoo <zainulrosikin981@gmail.com>
2023-11-17 10:15:12 +0700

    perintahtugas4

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
-<?php
+<?<php>
+$nama = "zainul"
 
+echo "Hallo, Selamat datang $nama </br>"; #petik dua
+echo 'Hallo, Selamat Datang $nama'; #pettik satu
 
-// variabel dan tipe data
-// variabel = digunakan untuk menyimpan data
-// Aturan variabel 'tidak boleh diawali dengan angka, tapi boleh mengandung angka'
-
-$nama = "Zainul";
 ?>
-
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <title>
-            Belajar php
-</title>
-</head>
-<body>
-    <h1>Hallo, selamat datang <?php echo $nama; ?></h1>
-
-</body>
-</html>
