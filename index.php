d82daa130f3fe4f12d72985553c7964c8f959a15
priasajoo <zainulrosikin981@gmail.com>
2023-11-17 10:30:33 +0700

    perintahtugas7

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
-$nama_depan = "ZAINUL";
-$nama_belakang = "ROSIKIN";
+$x = 10;
+$x += 2; #operator assignment +=
 
-echo $nama_depan . "" . $nama_belakang;
+echo $x "</br";
+
+$y = # tentunkan bebas
+$y -= # tentukan bebas
+echo $y "</br" #output operator assignment -=
+
+#buat untuk keseluruhan operator /=, %=, .=
+#buat menjadi 5 otput yang dapat keluar bersamaan
 
 ?>
