1f7be52f059170d1469ccb711e6672773b8d646f
priasajoo <zainulrosikin981@gmail.com>
2023-11-17 10:05:16 +0700

    perintahtugas3

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
+<?php
+
+
+// variabel dan tipe data
+// variabel = digunakan untuk menyimpan data
+// Aturan variabel 'tidak boleh diawali dengan angka, tapi boleh mengandung angka'
+
+$nama = "Zainul";
+?>
+
+<!DOCTYPE html>
+
 <html>
     <head>
-        <titlle>
-            Belajar php 
- </title>
+        <title>
+            Belajar php
+</title>
 </head>
 <body>
+    <h1>Hallo, selamat datang <?php echo $nama; ?></h1>
 
-<?php
-echo "<h1>Selamat Dayang, Zainul Rosikin</h1>";
-?>
 </body>
 </html>
